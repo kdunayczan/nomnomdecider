8f803dfc156aaa924a0b448839819e3831c8ce0e
Katie Dunayczan <katie.dunayczan@gmail.com>
2018-02-14 12:44:54 -0500

    Add open now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,22 +6,19 @@ class PagesController < ApplicationController
 		response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get(
 			'https://api.yelp.com/v3/businesses/search', 
 			params: {
-				term: "dinner",
+				term: "french",
 				location: "manhattan",
+				price: 1,
+				open_now: true,
 				limit: 5
 			})
 
 		@food_types = ["Italian", "American", "Chinese", "Japanese", "Mexican", "Seafood", "Mediterranean", "Thai", "Indian", "French", "Korean", "Barbeque", "Vegetarian"]
 
 		@locations = ["Manhattan", "Brooklyn", "Queens", "The Bronx", "Staten Island"]
-
-
 	end
 
 end
 
 
-# get response into a structure i can use
-# erb loop through results and print them up
-# change limit and find a way to tweak results
 
