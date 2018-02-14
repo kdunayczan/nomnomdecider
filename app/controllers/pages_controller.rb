9fe4a3c1c6feebf636cff6a5b351e3571df8f111
Katie Dunayczan <katie.dunayczan@gmail.com>
2018-02-14 12:33:36 -0500

    refactor drop down menus

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,18 @@ class PagesController < ApplicationController
 				location: "manhattan",
 				limit: 5
 			})
+
+		@food_types = ["Italian", "American", "Chinese", "Japanese", "Mexican", "Seafood", "Mediterranean", "Thai", "Indian", "French", "Korean", "Barbeque", "Vegetarian"]
+
+		@locations = ["Manhattan", "Brooklyn", "Queens", "The Bronx", "Staten Island"]
+
+
 	end
 
 end
+
+
+# get response into a structure i can use
+# erb loop through results and print them up
+# change limit and find a way to tweak results
+
